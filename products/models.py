4a88d26a0c6bd956135c8f857942e99a2722ee09
ARSHAD <arshadpathan1127@gmail.com>
2026-09-14 14:40:02 +0530

    added product model

--- products/models.py
+++ products/models.py
@@ -2,12 +2,43 @@ from django.db import models
 
 #Create your models here.
 class Product(models.Model):
-  Categories = models.TextChoices("Winter Collection","Summer Collection")
-  #Categories = {"Winter Collection","Summer Collection"}
   
-  name = models.CharField(max_length=34)
-  description = models.TextField(blank=True, max_length=300)
-  # pr_images = models.ImageField(blank=True, upload_to=products/)
-  category = models.TextField(blank=True, choices=Categories)
+  
+  name = models.CharField(default="", max_length=60)
+  
+  #pr_images = models.ImageField(blank=True, upload_to=products/)
+  
+  price = models.DecimalField(default="990", max_digits=10, decimal_places=2)
+
+  #category selection
+  Categories = (
+    ("Winter Collection","Winter Collection"),
+    ("Summer Collection","Summer Collection"),
+  )
+  
+  category = models.TextField(blank=False, choices=Categories)
+
+  #detail & description
+  details = models.TextField(default="", max_length=200)
+  
+  description = models.TextField(default="", max_length=300)
+  
+  #washcare detail
+  COTTON_501 = "this is cotton 501 washcare tips"
+  NYLON = "this is Nylon washcare tips"
+  
+  Washcare_tips = (
+    (COTTON_501, "COTTON 501"),
+    (NYLON, "NYLON"),
+  )
+
+  washcare = models.TextField(default="hii", choices=Washcare_tips)
+  
+  #shipping detail
+  shipping_policy = "read this before shipping"
+
+  shipping = models.TextField(default=shipping_policy)
+  
   pub_date = models.DateTimeField("date published")
+  
   
